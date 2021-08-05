441b392201847915023ce8f1ffd638e280901e23
Crowdbotics <team@crowdbotics.com>
2021-08-05 17:53:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "odd_salad_29527Identifier",
+  url: "https://odd_salad_29527Identifier.botics.co",
+  api: "https://odd_salad_29527Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
